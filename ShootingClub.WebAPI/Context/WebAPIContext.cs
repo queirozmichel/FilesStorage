b4119fa742e975e755d78c0aa7761edd29beb991
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-10-02 09:58:29 -0300

    Criação e configuração da classe de contexto

---
+++ ShootingClub.WebAPI/Context/WebAPIContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShootingClub.WebAPI.Models;
+using File = ShootingClub.WebAPI.Models.File;
+
+namespace ShootingClub.WebAPI.Context;
+
+public class WebAPIContext : DbContext
+{
+  public WebAPIContext(DbContextOptions<WebAPIContext> options) : base(options)
+  {
+
+  }
+
+  public DbSet<Client>? Clients { get; set; }
+  public DbSet<Address>? Addresses { get; set; }
+  public DbSet<File>? Files { get; set; }
+}
