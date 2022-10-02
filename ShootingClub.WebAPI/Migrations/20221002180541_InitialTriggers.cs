af226287393591d4d3d2fbda43dbd7398f250408
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-10-02 19:28:35 -0300

    Adição nas migrações, trigger para inserir a data na propriedade "ChangeDate"

---
+++ ShootingClub.WebAPI/Migrations/20221002180541_InitialTriggers.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShootingClub.WebAPI.Migrations
+{
+  public partial class InitialTriggers : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql(
+        "CREATE TRIGGER ClientsChangeDate ON Clients AFTER UPDATE, INSERT " +
+        "AS " +
+        "SET NOCOUNT ON " +
+        "BEGIN " +
+          "UPDATE Clients SET ChangeDate = CURRENT_TIMESTAMP FROM Clients " +
+          "INNER JOIN Inserted ON Clients.ClientId = Inserted.ClientId " +
+        "END");
+
+      migrationBuilder.Sql(
+        "CREATE TRIGGER AddressesChangeDate ON Addresses AFTER UPDATE, INSERT " +
+        "AS " +
+        "SET NOCOUNT ON " +
+        "BEGIN " +
+          "UPDATE Addresses SET ChangeDate = CURRENT_TIMESTAMP FROM Addresses " +
+          "INNER JOIN Inserted ON Addresses.AddressId = Inserted.AddressId " +
+        "END");
+
+      migrationBuilder.Sql(
+        "CREATE TRIGGER FilesChangeDate ON Files AFTER UPDATE, INSERT " +
+        "AS " +
+        "SET NOCOUNT ON " +
+        "BEGIN " +
+          "UPDATE Files SET ChangeDate = CURRENT_TIMESTAMP FROM Files " +
+          "INNER JOIN Inserted ON Files.FileId = Inserted.FileId " +
+        "END");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql("DROP TRIGGER ClientsChangeDate");
+      migrationBuilder.Sql("DROP TRIGGER AddressesChangeDate");
+      migrationBuilder.Sql("DROP TRIGGER FilesChangeDate");
+    }
+  }
+}
