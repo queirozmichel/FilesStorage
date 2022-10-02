34af1383004671e957c34736bc3389c083535c96
Michel Antunes <michelantunezqueiroz@gmail.com>
2022-10-02 19:25:15 -0300

    Adição nas migrações, a propriedade "ChangeDate" agora é nullable em todos os modelos

--- ShootingClub.WebAPI/Migrations/20221001171737_Initial.cs
+++ ShootingClub.WebAPI/Migrations/20221001171737_Initial.cs
@@ -5,98 +5,98 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ShootingClub.WebAPI.Migrations
 {
-    public partial class Initial : Migration
+  public partial class Initial : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Clients",
-                columns: table => new
-                {
-                    ClientId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Age = table.Column<int>(type: "int", nullable: false),
-                    Sex = table.Column<string>(type: "char(1)", nullable: false),
-                    CPF = table.Column<string>(type: "char(11)", nullable: false),
-                    PhoneNumber = table.Column<string>(type: "char(11)", nullable: false),
-                    ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Clients", x => x.ClientId);
-                });
+      migrationBuilder.CreateTable(
+          name: "Clients",
+          columns: table => new
+          {
+            ClientId = table.Column<int>(type: "int", nullable: false)
+                  .Annotation("SqlServer:Identity", "1, 1"),
+            Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+            Age = table.Column<int>(type: "int", nullable: false),
+            Sex = table.Column<string>(type: "char(1)", nullable: false),
+            CPF = table.Column<string>(type: "char(11)", nullable: false),
+            PhoneNumber = table.Column<string>(type: "char(11)", nullable: false),
+            ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Clients", x => x.ClientId);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Addresses",
-                columns: table => new
-                {
-                    AddressId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Street = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Number = table.Column<int>(type: "int", nullable: false),
-                    District = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    City = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
-                    State = table.Column<string>(type: "char(2)", nullable: false),
-                    CEP = table.Column<string>(type: "char(8)", nullable: false),
-                    ClientId = table.Column<int>(type: "int", nullable: false),
-                    ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
-                    table.ForeignKey(
-                        name: "FK_Addresses_Clients_ClientId",
-                        column: x => x.ClientId,
-                        principalTable: "Clients",
-                        principalColumn: "ClientId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+      migrationBuilder.CreateTable(
+          name: "Addresses",
+          columns: table => new
+          {
+            AddressId = table.Column<int>(type: "int", nullable: false)
+                  .Annotation("SqlServer:Identity", "1, 1"),
+            Street = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+            Number = table.Column<int>(type: "int", nullable: false),
+            District = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+            City = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+            State = table.Column<string>(type: "char(2)", nullable: false),
+            CEP = table.Column<string>(type: "char(8)", nullable: false),
+            ClientId = table.Column<int>(type: "int", nullable: false),
+            ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Addresses", x => x.AddressId);
+            table.ForeignKey(
+                      name: "FK_Addresses_Clients_ClientId",
+                      column: x => x.ClientId,
+                      principalTable: "Clients",
+                      principalColumn: "ClientId",
+                      onDelete: ReferentialAction.Cascade);
+          });
 
-            migrationBuilder.CreateTable(
-                name: "Files",
-                columns: table => new
-                {
-                    FileId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Extension = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
-                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
-                    ClientId = table.Column<int>(type: "int", nullable: false),
-                    ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Files", x => x.FileId);
-                    table.ForeignKey(
-                        name: "FK_Files_Clients_ClientId",
-                        column: x => x.ClientId,
-                        principalTable: "Clients",
-                        principalColumn: "ClientId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+      migrationBuilder.CreateTable(
+          name: "Files",
+          columns: table => new
+          {
+            FileId = table.Column<int>(type: "int", nullable: false)
+                  .Annotation("SqlServer:Identity", "1, 1"),
+            Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+            Extension = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+            Data = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+            ClientId = table.Column<int>(type: "int", nullable: false),
+            ChangeDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Files", x => x.FileId);
+            table.ForeignKey(
+                      name: "FK_Files_Clients_ClientId",
+                      column: x => x.ClientId,
+                      principalTable: "Clients",
+                      principalColumn: "ClientId",
+                      onDelete: ReferentialAction.Cascade);
+          });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Addresses_ClientId",
-                table: "Addresses",
-                column: "ClientId");
+      migrationBuilder.CreateIndex(
+          name: "IX_Addresses_ClientId",
+          table: "Addresses",
+          column: "ClientId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Files_ClientId",
-                table: "Files",
-                column: "ClientId");
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Addresses");
-
-            migrationBuilder.DropTable(
-                name: "Files");
-
-            migrationBuilder.DropTable(
-                name: "Clients");
-        }
+      migrationBuilder.CreateIndex(
+          name: "IX_Files_ClientId",
+          table: "Files",
+          column: "ClientId");
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "Addresses");
+
+      migrationBuilder.DropTable(
+          name: "Files");
+
+      migrationBuilder.DropTable(
+          name: "Clients");
+    }
+  }
 }
